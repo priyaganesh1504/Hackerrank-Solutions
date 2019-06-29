f6428f98eb912ac083bce53eddca61fcf314c1ab
priyaganesh1504 <42537456+priyaganesh1504@users.noreply.github.com>
2019-06-29 18:14:12 +0530

    Update apple and orange.java

--- apple and orange.java
+++ apple and orange.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String args[]){
         Scanner in=new Scanner(System.in);
-        int s=in.nextInt();
-        int t=in.nextInt();
-        int a=in.nextInt();
-        int b=in.nextInt();
-        int m=in.nextInt();
-        int n=in.nextInt();
-        int apples=0;
-        int oranges=0;
+        int s = in.nextInt(); //starting point of Sam's house location
+        int t = in.nextInt(); //ending point of Sam's house location
+        int a = in.nextInt();// location of the Apple tree
+        int b = in.nextInt();// location of the orange tree
+        int m = in.nextInt();//distances at which each apple falls from the tree
+        int n = in.nextInt();//distances at which each orange falls from the tree
+        int apples = 0;//the number of apples that fall on Sam's house
+        int oranges = 0;//the number of apples that fall on Sam's house
         for(int i=0;i<m;i++){
             int landing=in.nextInt()+a;
             if(landing>=s&&landing<=t){
